2e44cd4f1ae9b815934d6e84cff8b2733df99608
chloesun15 <chloesun15@gmail.com>
2020-08-24 21:00:23 -0400

    fix error and add new test

--- inventory-allocator/src/challenge_test.py
+++ inventory-allocator/src/challenge_test.py
@@ -5,6 +5,7 @@ def main():
     print("-- RUNNING UNIT TESTS -- ")
     exact_unit_test()
     no_allocation_unit_test()
+    split_warehouse_unit_test()
 
 
 def test_template(order, warehouse, output, test):
@@ -16,7 +17,7 @@ def test_template(order, warehouse, output, test):
     test_inventory = InventoryAllocator(test_order, test_warehouse)
     output = test_inventory.check_order()
     print("Expected output: " + str(expected_output))
-    print("Output: " + str(expected_output))
+    print("Output: " + str(output))
     assert output == expected_output
     print("" + test + " test passed.")
 
@@ -35,6 +36,14 @@ def no_allocation_unit_test():
     expected_output = []
 
     test_template(test_order, test_warehouse, expected_output, "No inventory")
+
+
+def split_warehouse_unit_test():
+    test_order = {"apple": 10}
+    test_warehouse = [{"name": "owd", "inventory": {"apple": 5}}, {"name": "dm", "inventory": {"apple": 5}}]
+    expected_output = [{"owd": {"apple": 5}}, {"dm": {"apple": 5}}]
+
+    test_template(test_order, test_warehouse, expected_output, "Split Warehouse")
 
 
 if __name__ == '__main__':
