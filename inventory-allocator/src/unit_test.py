51cae911982b0270efe0554a6ba3f1b3cca3e40d
chloesun15 <chloesun15@gmail.com>
2020-08-25 17:55:20 -0400

    expand/combine tests

--- inventory-allocator/src/unit_test.py
+++ inventory-allocator/src/unit_test.py
@@ -24,14 +24,12 @@ class InventoryAllocatorTest(unittest.TestCase):
         self.allocator.check_order()
         self.assertEqual(self.allocator.output, [{"owd": {"apple": 5}}, {"dm": {"apple": 5}}])
 
-    def test_one_empty_warehouse(self):
+    def test_empty_warehouses(self):
         self.allocator.set_order({"apple": 10})
         self.allocator.set_warehouse([{"name": "owd", "inventory": {}}, {"name": "dm", "inventory": {"apple": 10}}])
         self.allocator.check_order()
         self.assertEqual(self.allocator.output, [{"dm": {"apple": 10}}])
 
-    def test_only_empty_warehouses(self):
-        self.allocator.set_order({"apple": 10})
         self.allocator.set_warehouse([{"name": "owd", "inventory": {}}, {"name": "dm", "inventory": {}}])
         self.allocator.check_order()
         self.assertEqual(self.allocator.output, [])
@@ -42,11 +40,39 @@ class InventoryAllocatorTest(unittest.TestCase):
         self.allocator.check_order()
         self.assertEqual(self.allocator.output, [])
 
-    def test_empty_order(self):
+        self.allocator.set_order({"apple": 0})
+        self.allocator.set_warehouse([{"name": "owd", "inventory": {"apple": 20}}, {"name": "dm", "inventory": {"orange": 100}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [])
+
+    def test_empty_input(self):
         self.allocator.set_order({})
         self.allocator.set_warehouse([{"name": "owd", "inventory": {"apple": 0}}, {"name": "dm", "inventory": {"apple": 10}}])
         self.allocator.check_order()
         self.assertEqual(self.allocator.output, [])
+
+        self.allocator.set_order({"apple": 10})
+        self.allocator.set_warehouse([])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [])
+
+    def test_wrong_type(self):
+        self.allocator.set_order("")
+        self.allocator.set_warehouse([])
+        self.assertRaises(TypeError, self.allocator.check_order)
+
+        self.allocator.set_order({})
+        self.allocator.set_order(23)
+        self.assertRaises(TypeError, lambda: self.allocator.check_order())
+
+    def test_multi_item_order(self):
+        self.allocator.set_order({"apple": 10, "banana": 30})
+        self.allocator.set_warehouse(
+            [{"name": "owd", "inventory": {"apple": 5, "banana": 25}},
+             {"name": "dm", "inventory": {"apple": 5, "pineapple": 3, "banana": 5}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [{"owd": {"apple": 5, "banana": 25}},
+                                                 {"dm": {"apple": 5, "banana": 5}}])
 
 
 if __name__ == '__main__':
