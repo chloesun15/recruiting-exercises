0219c72aa55cfe910028c0cdad6e7c7ef48a9d28
chloesun15 <chloesun15@gmail.com>
2020-08-25 00:26:53 -0400

    add tests from old test file

--- inventory-allocator/src/unit_test.py
+++ inventory-allocator/src/unit_test.py
@@ -12,4 +12,42 @@ class InventoryAllocatorTest(unittest.TestCase):
         self.allocator.check_order()
         self.assertEqual(self.allocator.output, [{"owd": {"apple": 1}}])
 
-unittest.main()
+    def test_no_allocation(self):
+        self.allocator.set_order({"apple": 1})
+        self.allocator.set_warehouse([{"name": "owd", "inventory": {"apple": 0}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [])
+
+    def test_split_warehouse(self):
+        self.allocator.set_order({"apple": 10})
+        self.allocator.set_warehouse([{"name": "owd", "inventory": {"apple": 5}}, {"name": "dm", "inventory": {"apple": 5}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [{"owd": {"apple": 5}}, {"dm": {"apple": 5}}])
+
+    def test_one_empty_warehouse(self):
+        self.allocator.set_order({"apple": 10})
+        self.allocator.set_warehouse([{"name": "owd", "inventory": {}}, {"name": "dm", "inventory": {"apple": 10}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [{"dm": {"apple": 10}}])
+
+    def test_only_empty_warehouses(self):
+        self.allocator.set_order({"apple": 10})
+        self.allocator.set_warehouse([{"name": "owd", "inventory": {}}, {"name": "dm", "inventory": {}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [])
+
+    def test_zero_items(self):
+        self.allocator.set_order({"apple": 10})
+        self.allocator.set_warehouse([{"name": "owd", "inventory": {"apple": 0}}, {"name": "dm", "inventory": {"apple": 0}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [])
+
+    def test_empty_order(self):
+        self.allocator.set_order({})
+        self.allocator.set_warehouse([{"name": "owd", "inventory": {"apple": 0}}, {"name": "dm", "inventory": {"apple": 10}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
