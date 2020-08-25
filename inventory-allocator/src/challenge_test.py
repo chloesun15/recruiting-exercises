ae1e9caf5c028bdc90d4fd8d3c5597bdb867d6ee
chloesun15 <chloesun15@gmail.com>
2020-08-24 20:55:32 -0400

    create testing template

--- inventory-allocator/src/challenge_test.py
+++ inventory-allocator/src/challenge_test.py
@@ -7,32 +7,34 @@ def main():
     no_allocation_unit_test()
 
 
+def test_template(order, warehouse, output, test):
+    print("\n" + test + " test running...")
+    test_order = order
+    test_warehouse = warehouse
+    expected_output = output
+
+    test_inventory = InventoryAllocator(test_order, test_warehouse)
+    output = test_inventory.check_order()
+    print("Expected output: " + str(expected_output))
+    print("Output: " + str(expected_output))
+    assert output == expected_output
+    print("" + test + " test passed.")
+
+
 def exact_unit_test():
-    print("\nExact inventory match test running...\n")
     test_order = {"apple": 1}
     test_warehouse = [{"name": "owd", "inventory": {"apple": 1}}]
     expected_output = [{"owd": {"apple": 1}}]
 
-    test_inventory = InventoryAllocator(test_order, test_warehouse)
-    output = test_inventory.check_order()
-    print("Expected output: " + str(expected_output))
-    print("Output: " + str(expected_output))
-    assert output == expected_output
-    print("\nExact inventory match test passed.")
+    test_template(test_order, test_warehouse, expected_output, "Exact inventory match")
 
 
 def no_allocation_unit_test():
-    print("\nNo allocation match test running...\n")
     test_order = {"apple": 1}
     test_warehouse = [{"name": "owd", "inventory": {"apple": 0}}]
     expected_output = []
 
-    test_inventory = InventoryAllocator(test_order, test_warehouse)
-    output = test_inventory.check_order()
-    print("Expected output: " + str(expected_output))
-    print("Output: " + str(expected_output))
-    assert output == expected_output
-    print("\nNo allocation match test passed.")
+    test_template(test_order, test_warehouse, expected_output, "No inventory")
 
 
 if __name__ == '__main__':
