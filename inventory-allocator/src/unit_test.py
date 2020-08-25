94507e60c915b2668a7a36d3595d6a7c321eca47
chloesun15 <chloesun15@gmail.com>
2020-08-25 00:19:04 -0400

    create test for exact allocation

--- inventory-allocator/src/unit_test.py
+++ inventory-allocator/src/unit_test.py
@@ -1,3 +1,15 @@
 from challenge import *
-from unittest import TestCase
+import unittest
 
+
+class InventoryAllocatorTest(unittest.TestCase):
+    def setUp(self):
+        self.allocator = InventoryAllocator()
+
+    def test_exact_allocation(self):
+        self.allocator.set_order({"apple": 1})
+        self.allocator.set_warehouse([{"name": "owd", "inventory": {"apple": 1}}])
+        self.allocator.check_order()
+        self.assertEqual(self.allocator.output, [{"owd": {"apple": 1}}])
+
+unittest.main()
