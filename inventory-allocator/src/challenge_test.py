851a7d8975e091f5ddca9119a3338061f4eabbf8
chloesun15 <chloesun15@gmail.com>
2020-08-24 20:46:04 -0400

    new print statements

--- inventory-allocator/src/challenge_test.py
+++ inventory-allocator/src/challenge_test.py
@@ -2,17 +2,22 @@ from challenge import *
 
 
 def main():
-    print("test running")
+    print("-- RUNNING UNIT TESTS -- ")
     exact_unit_test()
 
 
 def exact_unit_test():
+    print("\nExact inventory match test running...\n")
     test_order = {"apple": 1}
     test_warehouse = [{"name": "owd", "inventory": {"apple": 1}}]
+    expected_output = [{"owd": {"apple": 1}}]
 
     test_inventory = InventoryAllocator(test_order, test_warehouse)
     output = test_inventory.check_order()
-    assert output == [{"owd": {"apple": 1}}]
+    print("Expected output: " + str(expected_output))
+    print("Output: " + str(expected_output))
+    assert output == expected_output
+    print("\nExact inventory match test passed.")
 
 
 if __name__ == '__main__':
